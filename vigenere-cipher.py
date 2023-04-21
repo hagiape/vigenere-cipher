72ac70fe1d578654962fcfcd3d640f891e5d036a
hagiape <g_chrisf@protonmail.com>
2023-04-21 17:58:08 +0800

    first commit - comments as guide for coding

---
+++ vigenere-cipher.py
@@ -0,0 +1,11 @@
+# alphabet list
+# function for catching errors
+# input variable for the message
+# function init for message
+# input variable for the key
+# function init for key var.
+# placeholder variables for the mod value and encrypted message
+# while loop to correspond key chars. with message chars.
+# remove extra chars. of key before adding the values of chars. of message var.
+# for loop
+#execution

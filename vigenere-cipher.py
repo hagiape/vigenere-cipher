d77735027fc5131df7c6ad0ec03882fc31727d87
hagiape <g_chrisf@protonmail.com>
2023-04-21 18:02:50 +0800

    input and output variables plus init. of catch-error functions

--- vigenere-cipher.py
+++ vigenere-cipher.py
@@ -6,10 +6,16 @@ def catch_errors(text_input):
         print('Error: You need to put uppercase letters only! Try to run the program again.')
         quit()
 # input variable for the message
+message = input('Message (only uppercase letters and no spaces): ')
 # function init for message
+catch_errors(message)
 # input variable for the key
+key = input('Key (all capital letters and no spaces): ')
 # function init for key var.
+catch_errors(key)
 # placeholder variables for the mod value and encrypted message
+encrypted_message = ''
+mod = 0
 # while loop to correspond key chars. with message chars.
 # remove extra chars. of key before adding the values of chars. of message var.
 # for loop

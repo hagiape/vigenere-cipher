f6d06004d1c67f029a416d4c82553f294cb4b7c3
hagiape <g_chrisf@protonmail.com>
2023-04-21 18:00:49 +0800

    defined a function for catching errors

--- vigenere-cipher.py
+++ vigenere-cipher.py
@@ -1,6 +1,10 @@
-# list of chars. in the alphabet
+# list of chars. in the English alphabet
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 # function for catching errors
+def catch_errors(text_input):
+    if text_input.isnumeric() or text_input.islower() or not text_input.isalpha():
+        print('Error: You need to put uppercase letters only! Try to run the program again.')
+        quit()
 # input variable for the message
 # function init for message
 # input variable for the key
